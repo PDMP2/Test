e727ce0ac6f182c339c4b16cdccaa0f3fb8f5845
PDMP2 <PDMP2@yandex.ru>
2016-03-29 10:03:57 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	public static void main(String[] args){
+		
+		System.out.println("Hello world!");
+		
+		if(test1() && test2()){
+			System.out.println("Yes");
+		}
+		
+	}
+	
+	public static boolean test1(){
+		System.out.println("TEST 1");
+		return true;
+	}
+	
+	public static boolean test2(){
+		System.out.println("TEST 2");
+		return false;
+	}
+}
