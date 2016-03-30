7719c6efc257a48065d02c65f54773ffd3d30ecd
PDMP2 <postalfa1@gmail.com>
2016-03-30 14:13:25 +0300

    Parameters

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 			System.out.println("Yes");
 		}
 		
+		add(1,2,3);
+		
 		System.out.println("END");
 		
 	}
@@ -20,5 +22,9 @@ public class Main {
 	public static boolean test2(){
 		System.out.println("TEST 2");
 		return false;
+	}
+	
+	public static void add(int... list){
+		System.out.println(list.length);
 	}
 }
